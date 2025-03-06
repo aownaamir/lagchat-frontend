0a35de68e6919667218c246f79a369bfc6412c19
Aown-Aamir <aownaamirkips@gmail.com>
2025-03-06 08:41:27 +0500

    chat started

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,25 @@ import { createBrowserRouter } from "react-router-dom";
 function Home() {
   return (
     <div className="">
-      <div className="gap-2 m-4 inline-block">
-        <input
-          type="text"
-          placeholder="Reciever IP"
-          className="border border-black py-2 px-3"
-        />
-        <button className="border border-black py-2 px-3 ml-2">Connect</button>
+      <div className="buttons-container flex flex-col">
+        <button className="border border-black p-2 rounded-lg">
+          Global chat log
+        </button>
+        <button className="border border-black p-2 rounded-lg">
+          global authenticated chat log
+        </button>
+        <button className="border border-black p-2 rounded-lg">
+          specific user chat log
+        </button>
+        <button className="border border-black p-2 rounded-lg">
+          specific authenticated user chat log
+        </button>
+        <button className="border border-black p-2 rounded-lg">
+          server chat log
+        </button>
+        <button className="border border-black p-2 rounded-lg">
+          server authenticated chat log
+        </button>
       </div>
     </div>
   );
